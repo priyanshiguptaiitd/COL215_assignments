9f031ed08db0de4a517f7565c28e8491b24a1d10
Yash Rawat <cs5230334@iitd.ac.in>
2024-08-19 01:36:48 +0530

    V2 of Packer

--- SW_Assignment_1_Files/Code/helper.py
+++ SW_Assignment_1_Files/Code/helper.py
@@ -1,12 +1,13 @@
 cmd_command_0 = r"cd C:\Users\YASH\OneDrive\Desktop\IIT D\Sem 3\Courses\COL215\Practical Work\SW Assignments\SW_Assignment_1\SW_Assignment_1_Files\Sample_Test_Cases"
 cmd_command_1 = r"python visualize_gates.py output.txt input.txt 450 500"
-import numpy as np
+# import numpy as np
 
-import random as r 
-for j in range(2,6):
-    with open(f"tc{j}_100.txt","w") as file:
-        for i in range(1,101):
-            file.write(f"g{i} {r.randint(1,100)} {r.randint(1,100)} \n")
+# import random as r 
+# for j in range(1,2):
+#     with open(f"tc{j}_10000.txt","w") as file:
+#         for i in range(1,10001):
+#             file.write(f"g{i} {r.randint(1,100)} {r.randint(1,100)} \n")
+
 
 
 
