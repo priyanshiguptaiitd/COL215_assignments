abcdf77acf0b0117c7a4ef68a07ad80aa36f2a96
Yash Rawat <cs5230334@iitd.ac.in>
2024-08-29 16:29:15 +0530

    Added TB Code

---
+++ HW Assignments/HW Assignment 2/HW_Assignment_2_Files/VHD Files/tb_Files/tb_7Seg_Display.vhd
@@ -0,0 +1,124 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_7Seg_Display is
+end tb_7Seg_Display;
+
+architecture tb of tb_7Seg_Display is
+    component display_seven_seg is
+        Port (
+            clock_in : in STD_LOGIC; -- 100 MHz input clock
+            reset_timer : in STD_LOGIC; -- Reset signal (Resetting the internal signals to known states)
+            d0: in std_logic_vector(3 downto 0);
+            d1: in std_logic_vector(3 downto 0);
+            d2: in std_logic_vector(3 downto 0);
+            d3: in std_logic_vector(3 downto 0);
+            an : out STD_LOGIC_VECTOR (3 downto 0); -- Anodes signal for display
+            seg : out STD_LOGIC_VECTOR (6 downto 0) -- Cathodes signal for display
+        );
+    end component;
+    signal clock_in : std_logic;
+    signal reset_timer : std_logic;
+    signal d0 : std_logic_vector(3 downto 0);
+    signal d1 : std_logic_vector(3 downto 0);
+    signal d2 : std_logic_vector(3 downto 0);
+    signal d3 : std_logic_vector(3 downto 0);
+    signal an : std_logic_vector(3 downto 0);
+    signal seg : std_logic_vector(6 downto 0);
+begin
+    UUT : display_seven_seg port map (
+        clock_in => clock_in,
+        reset_timer => reset_timer,
+        d0 => d0,
+        d1 => d1,
+        d2 => d2,
+        d3 => d3,
+        an => an,
+        seg => seg
+    );
+
+    clock_proc: process
+    begin
+        while true loop
+            clock_in <= '0';
+            wait for 5 ns;
+            clock_in <= '1';
+            wait for 5 ns;
+        end loop;
+    end process;
+    
+    sim_proc: process
+    begin
+        reset_timer <= '1';
+        d0 <= "0000";
+        d1 <= "0000";
+        d2 <= "0000";
+        d3 <= "0000";
+        wait for 100 ns;
+        reset_timer <= '0';
+        d0 <= "0000";
+        d1 <= "0000";
+        d2 <= "0000";
+        d3 <= "0000";
+        wait for 100 ns;
+        d0 <= "0001";
+        d1 <= "0001";
+        d2 <= "0001";
+        d3 <= "0001";
+        wait for 100 ns;
+        d0 <= "0010";
+        d1 <= "0010";
+        d2 <= "0010";
+        d3 <= "0010";
+        wait for 100 ns;
+        d0 <= "0011";
+        d1 <= "0011";
+        d2 <= "0011";
+        d3 <= "0011";
+        wait for 100 ns;
+        d0 <= "0100";
+        d1 <= "0100";
+        d2 <= "0100";
+        d3 <= "0100";
+        wait for 100 ns;
+        d0 <= "0101";
+        d1 <= "0101";
+        d2 <= "0101";
+        d3 <= "0101";
+        wait for 100 ns;
+        d0 <= "0110";
+        d1 <= "0110";
+        d2 <= "0110";
+        d3 <= "0110";
+        wait for 100 ns;
+        d0 <= "0111";
+        d1 <= "0111";
+        d2 <= "0111";
+        d3 <= "0111";
+        wait for 100 ns;
+        d0 <= "1000";
+        d1 <= "1000";
+        d2 <= "1000";
+        d3 <= "1000";
+        wait for 100 ns;
+        d0 <= "1001";
+        d1 <= "1001";
+        d2 <= "1001";
+        d3 <= "1001";
+        wait for 100 ns;
+        d0 <= "1010";
+        d1 <= "1010";
+        d2 <= "1010";
+        d3 <= "1010";
+        wait for 100 ns;
+
+        end tb;
---
+++ HW Assignments/HW Assignment 2/HW_Assignment_2_Files/VHD Files/tb_Files/tb_MUX_4BIT.vhd
@@ -0,0 +1,31 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity tb_MUX_4BIT is
+end tb_MUX_4BIT;
+
+architecture tb of tb_MUX_4BIT is
+    component seven_seg_decoder_hex is
+        port (
+            dec_in : in std_logic_vector(3 downto 0);
+            dec_out : out std_logic_vector(6 downto 0)
+        );
+    end component;
+    signal dec_in: std_logic_vector(3 downto 0);
+    signal dec_out : std_logic_vector(6 downto 0);
+begin
+    UUT : seven_seg_decoder_hex port map (dec_in=>dec_in,dec_out=>dec_out);
+    dec_in  <= '0000', '0001' after 20 ns, '0010' after 40 ns, '0011' after 80 ns ,
+               '0100' after 100 ns, '0101' after 120 ns, '0110' after 140 ns, '0111' after 160 ns,
+               '1000' after 180 ns, '1001' after 200 ns, '1010' after 220 ns, '1011' after 240 ns,
+               '1100' after 260 ns, '1101' after 280 ns, '1110' after 300 ns, '1111' after 320 ns;
+end tb;
