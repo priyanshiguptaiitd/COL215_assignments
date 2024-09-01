0692c75372c60cbe4dee30b8ea6c6b8d2bcfc64d
Yash Rawat <cs5230334@iitd.ac.in>
2024-09-01 14:24:51 +0530

    Improved the TestBench

--- HW Assignments/HW Assignment 2/HW_Assignment_2_Files/VHD Files/tb_Files/tb_7Seg_Display.vhd
+++ HW Assignments/HW Assignment 2/HW_Assignment_2_Files/VHD Files/tb_Files/tb_7Seg_Display.vhd
@@ -35,90 +35,22 @@ architecture tb of tb_7Seg_Display is
     signal an : std_logic_vector(3 downto 0);
     signal seg : std_logic_vector(6 downto 0);
 begin
-    UUT : display_seven_seg port map (
-        clock_in => clock_in,
-        reset_timer => reset_timer,
-        d0 => d0,
-        d1 => d1,
-        d2 => d2,
-        d3 => d3,
-        an => an,
-        seg => seg
-    );
-
+    UUT : display_seven_seg port map (clock_in => clock_in, reset_timer => reset_timer,
+                                      d0 => d0, d1 => d1, d2 => d2, d3 => d3, an => an, seg => seg);
     clock_proc: process
     begin
-        while true loop
-            clock_in <= '0';
+        while now < 40000 ns loop
+            clk_in_tb <= '0';
             wait for 5 ns;
-            clock_in <= '1';
+            clk_in_tb <= '1';
             wait for 5 ns;
         end loop;
+        wait;
     end process;
-    
-    sim_proc: process
-    begin
-        reset_timer <= '1';
-        d0 <= "0000";
-        d1 <= "0000";
-        d2 <= "0000";
-        d3 <= "0000";
-        wait for 100 ns;
-        reset_timer <= '0';
-        d0 <= "0000";
-        d1 <= "0000";
-        d2 <= "0000";
-        d3 <= "0000";
-        wait for 100 ns;
-        d0 <= "0001";
-        d1 <= "0001";
-        d2 <= "0001";
-        d3 <= "0001";
-        wait for 100 ns;
-        d0 <= "0010";
-        d1 <= "0010";
-        d2 <= "0010";
-        d3 <= "0010";
-        wait for 100 ns;
-        d0 <= "0011";
-        d1 <= "0011";
-        d2 <= "0011";
-        d3 <= "0011";
-        wait for 100 ns;
-        d0 <= "0100";
-        d1 <= "0100";
-        d2 <= "0100";
-        d3 <= "0100";
-        wait for 100 ns;
-        d0 <= "0101";
-        d1 <= "0101";
-        d2 <= "0101";
-        d3 <= "0101";
-        wait for 100 ns;
-        d0 <= "0110";
-        d1 <= "0110";
-        d2 <= "0110";
-        d3 <= "0110";
-        wait for 100 ns;
-        d0 <= "0111";
-        d1 <= "0111";
-        d2 <= "0111";
-        d3 <= "0111";
-        wait for 100 ns;
-        d0 <= "1000";
-        d1 <= "1000";
-        d2 <= "1000";
-        d3 <= "1000";
-        wait for 100 ns;
-        d0 <= "1001";
-        d1 <= "1001";
-        d2 <= "1001";
-        d3 <= "1001";
-        wait for 100 ns;
-        d0 <= "1010";
-        d1 <= "1010";
-        d2 <= "1010";
-        d3 <= "1010";
-        wait for 100 ns;
 
-        end tb;
+    d0 <= "0000", "0001" after 1024 ns, "0010" after 2048 ns;
+    d1 <= "0000", "0001" after 1024 ns, "0010" after 2048 ns;
+    d2 <= "0000", "0001" after 1024 ns, "0010" after 2048 ns;
+    d3 <= "0000", "0001" after 1024 ns, "0010" after 2048 ns;
+
+    end tb;
