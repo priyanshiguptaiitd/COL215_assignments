efab0365c2024b94d51a4c42b9663c1a39167196
priyanshiguptaiitd <priyanshiguptaiitd@gmail.com>
2024-10-14 16:30:47 +0530

    Update bitwise_xor.vhd

--- HW Assignments/HW Assignment 3/Code/bitwise_xor.vhd
+++ HW Assignments/HW Assignment 3/Code/bitwise_xor.vhd
@@ -1,1 +1,23 @@
+library ieee;
+use ieee.std_logic_1164.all;
 
+entity bitwise_xor is
+	port (
+		input_byte : in std_logic_vector(7 downto 0);
+		output_byte : out std_logic_vector(7 downto 0)
+	);
+end bitwise_xor;
+
+architecture behavioral of inv_sbox is
+
+begin
+	lut : process (input_byte) is
+	begin
+		case input_byte is
+			
+			when others => null; -- GHDL complains without this statement
+		end case;
+
+	end process lut;
+
+end architecture behavioral;
