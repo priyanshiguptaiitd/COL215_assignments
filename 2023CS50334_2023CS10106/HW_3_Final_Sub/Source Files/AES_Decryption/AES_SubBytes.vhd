2ab3af0bce8e594f71ae819a670af8cdc1331748
Yash Rawat <cs5230334@iitd.ac.in>
2024-11-12 02:00:48 +0530

    Final Commit to this Repo

---
+++ 2023CS50334_2023CS10106/HW_3_Final_Sub/Source Files/AES_Decryption/AES_SubBytes.vhd
@@ -0,0 +1,56 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+-- Accepts an 8-bit Address as input and returns the corresponding value from the S-Box
+
+entity AES_SubBytes is
+    Generic ( 
+              S : integer := 1;  -- Data width (bits per entry)
+              N : integer := 8   -- Number of elements to be processed
+            );
+    
+    Port(   
+            -- clk : in std_logic;
+            data_input : in std_logic_vector(S * N - 1 downto 0);
+            en : in std_logic;
+            data_output : out std_logic_vector(S * N - 1 downto 0)
+    );
+
+end AES_SubBytes;
+
+architecture Behavioral of AES_SubBytes is
+
+    component ROM_SBOX
+        Generic ( 
+            DATA_WIDTH : integer := 8;
+            ADDR_WIDTH : integer := 8
+        );
+        Port ( 
+            addr : in STD_LOGIC_VECTOR(ADDR_WIDTH - 1 downto 0);
+            data_out : out STD_LOGIC_VECTOR(DATA_WIDTH - 1 downto 0)
+        );
+    end component;
+
+    -- signal i : integer range 0 to N - 1 := 0;
+    -- signal is_done : std_logic := '0';
+    signal addr_in : std_logic_vector(7 downto 0) := (others => '0');
+    signal data_out : std_logic_vector(7 downto 0) := (others => '0');
+
+begin
+    
+    uut: ROM_SBOX
+        Generic map (
+            DATA_WIDTH => 8,
+            ADDR_WIDTH => 8
+        )
+        Port map (
+            addr => addr_in,
+            data_out => data_out
+        );
+    
+    addr_in <= data_input;  -- Extract byte for address lookup
+    data_output <= data_out;  -- Assign output byte by byte
+    
+    
+end Behavioral;
